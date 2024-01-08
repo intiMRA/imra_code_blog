c1676bf1c16fd68fd36ce266e1ed20dc320f5626
inti <inti.albuquerque@trademe.co.nz>
2024-01-08 13:16:16 +1300

    changed description

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,14 +7,7 @@ const Intro = () => {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A blog where I post coding articles{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+        A blog where I post coding articles
       </h4>
     </section>
   )
