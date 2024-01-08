63d9b11d0dfddc5734b1b3752d8e3680ee982ae3
inti <inti.albuquerque@trademe.co.nz>
2024-01-08 13:11:53 +1300

    changed description

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,7 @@ const Intro = () => {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        A blog where I post coding articles{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
