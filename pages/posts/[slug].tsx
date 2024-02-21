ab901e697424b37500af92fa9295fb82f44967c6
inti <inti.albuquerque@trademe.co.nz>
2024-02-21 14:51:29 +1300

    update tag

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -37,7 +37,7 @@ export default function Post({ post, morePosts, preview }: Props) {
                 <title>{title}</title>
                 <meta property="og:image" content={post.ogImage.url} />
                 <meta property='twitter:image' content={post.ogImage.url} />
-                <meta name="twitter:image:src" content={post.ogImage.url}/>
+                <meta name="twitter:image:src" content="https://github.com/intiMRA/imra_code_blog/blob/main/public/favicon/safari-pinned-tab.svg"/>
                 <meta name="twitter:card" content='summary_large_image' />
               </Head>
               <PostHeader
