98800cbf6c08802c7bd1868c5c1b25ebb7ae90f2
inti <inti.albuquerque@trademe.co.nz>
2024-02-21 14:13:32 +1300

    removed unused path

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
-export const EXAMPLE_PATH = 'blog-starter'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
   'https://github.com/intiMRA/imra_code_blog/blob/main/public/favicon/safari-pinned-tab.svg'
